ea4595d33ed6b8b7131e190910bbdce602a8fe90
erya <hang3126@sina.com>
2016-10-28 14:29:10 +0800

    add mordal and back to top button

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Scroll to Top
+$(".totop").hide();
+
+$(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop()>300) {
+            $('.totop').slideDown();
+        } else {
+            $('.totop').slideUp();
+        }
+    });
+    $('.totop a').click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({scrollTop: 0}, 500);
+    });
+});
